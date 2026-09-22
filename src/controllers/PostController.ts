14e9e31836ae6af64b9c7e606f4f5e828e26e871
Alexander Forsanker <forale@macforale-2607.jth-pers3.hj.se>
2026-09-22 09:35:16 +0200

    Separated the Views

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -1,6 +1,11 @@
+import type { LoaderFunctionArgs } from "react-router-dom";
 import type { Post } from "../models/Post";
 import { getPosts } from "../services/PostService";
 
-export function getAllPosts(): Post[] {
-  return getPosts();
+export function loadPosts(): Post[] {
+  return getPosts()
+}
+
+export function loadPost({ params }: LoaderFunctionArgs) {
+  return getPosts().find((post) => post.id === Number(params.postId))
 }
